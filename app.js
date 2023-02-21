c15f42289a1b0d228599f5afa62aa964c9403ae3
luandev12 <luanitdev@gmail.com>
2023-02-21 14:57:57 +0700

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const routes = require('./routes/route.js');
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(morgan('dev'));
 
 app.get('/', routes.home);
 
